f87415b4cfc5fd54c3531ca25f9fc14a1cb41fb8
SamarthBM <samarthbm18@gmail.com>
2021-06-29 14:17:19 +0530

    Adding UC2

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -4,6 +4,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("HELLO, THIS IS A GIT DEMO, HOW ARE YOU?");
 		System.out.println("Hello UC1");
+		System.out.println("Hello UC2");
 
 	}
 
